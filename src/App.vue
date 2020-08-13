d6586b56c3f540a8f474fdd88b0ceaf7ee8f5636
Antonio Toriz <torizantonio15@gmail.com>
2020-08-13 01:26:38 -0700

    added method to add

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
     <h1>To-Do List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
     <ul>
       <li v-for="item in ToDoItems" :key="item.id">
-        <!-- <ToDoItem /> -->
-        <to-do-item v-bind:label="item.label" v-bind:done="item.done" v-bind:id="item.id"></to-do-item>
+        <to-do-item
+          v-bind:label="item.label"
+          v-bind:done="item.done"
+          v-bind:id="item.id"
+        ></to-do-item>
       </li>
     </ul>
   </div>
@@ -12,22 +16,38 @@
 
 <script>
 import ToDoItem from './components/ToDoItem.vue';
+import ToDoForm from './components/ToDoForm';
 import uniqueId from 'lodash.uniqueid';
 
 export default {
   name: 'app',
   components: {
     ToDoItem,
+    ToDoForm,
   },
   data() {
     return {
       ToDoItems: [
         { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
-        { id: uniqueId('todo-'), label: 'Create a Vue project with the CLI', done: true, },
+        {
+          id: uniqueId('todo-'),
+          label: 'Create a Vue project with the CLI',
+          done: true,
+        },
         { id: uniqueId('todo-'), label: 'Have fun', done: true },
         { id: uniqueId('todo-'), label: 'Create a to-do list', done: false },
       ],
     };
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      console.log('To-Do added', toDoLabel);
+      this.ToDoItems.push({
+        id: uniqueId('todo-'),
+        label: toDoLabel,
+        done: false,
+      });
+    },
   },
 };
 </script>
